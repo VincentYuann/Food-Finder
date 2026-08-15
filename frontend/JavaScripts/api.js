c3449ee63b9a2ff3cbf8414bf7bc7ddd08c50840
Vincent Yuan <vincentyuan1020@gmail.com>
2026-08-15 01:05:14 -0400

    added backend url

--- frontend/JavaScripts/api.js
+++ frontend/JavaScripts/api.js
@@ -8,7 +8,7 @@ let API_BASE_URL;
 if (window.location.hostname === 'localhost') {
     API_BASE_URL = 'http://localhost:5000';
 } else {
-    API_BASE_URL = 'https://your-backend-api-url.onrender.com';
+    API_BASE_URL = 'https://food-finder-lk9t.onrender.com';
 }
 
 /**
